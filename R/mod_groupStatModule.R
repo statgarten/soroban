5e44fb35e8ec228274e46f7cedb022b3a5e6dca3
Jinhwan Kim <hwanistic@gmail.com>
2023-10-24 22:59:32 +0900

    feat: add group metrics round / download
    close #11

--- R/mod_groupStatModule.R
+++ R/mod_groupStatModule.R
@@ -8,9 +8,11 @@
 #'
 #'
 #' @importFrom reactable reactableOutput
+#' @importFrom shinyjs hidden useShinyjs
 mod_groupStatModule_ui <- function(id) {
   ns <- NS(id)
   fluidRow(
+    useShinyjs(),
     column( # Result Area
       width = 9,
       reactableOutput(
@@ -33,21 +35,32 @@ mod_groupStatModule_ui <- function(id) {
         choices = c("mean", "median", "sd", "iqr" = "IQR", "mad", "min", "max"),
         width = "100%"
       ), # not quantile, first, last, nth, n, n_distint
+      numericInput(
+        inputId = ns("round"),
+        label = "Round",
+        value = 2,
+        min = 0,
+        max = 5,
+        step = 1
+      ),
       actionButton( # Main Action
         inputId = ns("build"),
         label = "build",
         style = "font-weight: bold; width: 100%; background: #004B4D; border-radius: 0; color: white; border: 0;"
+      ),
+      shinyjs::hidden(
+        div(
+          id = ns("hidden"),
+          tags$button(
+            tagList(fontawesome::fa("download"), "Download as CSV"),
+            class = "btn btn-default shiny-download-link",
+            style = "margin-top: 1em; width: 100%; font-weight: bold; width: 100%; background: #004B4D; border-radius: 0; color: white; border: 0;",
+            onclick = paste0("Reactable.downloadDataCSV('", id, "-myTable', 'downloads.csv')")
+          )
+        )
       )
     )
   )
-
-  ## NOT USED DOWNLOAD BUTTON
-
-  # tags$button(
-  #   tagList(fontawesome::fa("download"), "Download as CSV"),
-  #   class = "btn btn-default shiny-download-link",
-  #   onclick = "Reactable.downloadDataCSV('groupStatModule_1-myTable', 'downloads.csv')"
-  # )
 }
 
 #' @title Shiny Module for data transformation
@@ -59,6 +72,7 @@ mod_groupStatModule_ui <- function(id) {
 #' @return transformed data
 #' @importFrom dplyr group_by summarise across
 #' @importFrom reactable reactable renderReactable
+#' @importFrom shinyjs show
 mod_groupStatModule_server <- function(id, inputData) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
@@ -77,10 +91,15 @@ mod_groupStatModule_server <- function(id, inputData) {
       data <- inputData()
       v <- data %>%
         group_by(across(input$groups)) %>%
-        summarise(across(names(Filter(is.numeric, data)), match.fun(input$func), na.rm = TRUE)) %>%
-        reactable()
+        summarise(across(names(Filter(is.numeric, data)), match.fun(input$func), na.rm = TRUE))
 
-      output$myTable <- renderReactable(v)
+      for (i in 2:ncol(v)) {
+        v[, i] <- round(v[, i], digits = input$round)
+      }
+
+      output$myTable <- renderReactable(reactable(v))
+
+      shinyjs::show(id = "hidden")
     })
   })
 }
